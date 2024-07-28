36349c04c4e1a444c8bb27b726ef9f402db3c46b
go-arnold <arnoldciku@gmail.com>
2024-07-28 23:54:30 +0200

    txt

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-python3.12 -m pip install -r requirements.text
+#!/bin/bash
+
+#Build the project
+echo "Building the project"
+python3.12 -m pip install -r requirements.txt
+
+echo "Collect static files"
 python3.12 manage.py collectstatic 
