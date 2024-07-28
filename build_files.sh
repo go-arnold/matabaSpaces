4fa707b6295bee8ba0b92f864b89f192767eca51
go-arnold <arnoldciku@gmail.com>
2024-07-29 00:44:22 +0200

    txt5

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,8 @@
 echo "Building the project"
 python3.12 -m pip install -r requirements.txt
 
+export PATH="/python312/bin:$PATH"
+
+
 echo "Collect static files"
 python3.12 manage.py collectstatic 
--- matabaSpaces/settings.py
+++ matabaSpaces/settings.py
@@ -67,7 +67,8 @@ CELERY_TIMEZONE = 'Africa/Kigali'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+   
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
