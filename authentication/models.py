9800e4f7b2c548683324d6b8a0a76b0328f3c09c
go-arnold <arnoldciku@gmail.com>
2024-08-03 07:08:53 +0200

    final

--- authentication/models.py
+++ authentication/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     
     email=models.EmailField(max_length=100,unique=True,null=True)
     phone_number = models.IntegerField(unique=True,null=True)
-    photo = models.ImageField(upload_to='user_images/', default='user_images/avatar.png')   
+    photo = models.ImageField(upload_to='user_images/', default='user_images/avatar')   
     bio=models.CharField(null=True,blank=True,max_length=250)
     
     USERNAME_FIELD='email'
